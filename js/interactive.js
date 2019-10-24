c7d7f87b7875f6e15b1df34841580d8fd9e27cf8
morozzova_kraken <lubasha-s@yandex.ru>
2019-10-24 20:22:17 +0300

    Adaptivity ver. 1.0 is ready.

---
+++ js/interactive.js
@@ -0,0 +1,297 @@
+ var hov1 = document.querySelector('#scheme_item_01');
+        $('#scheme_01').hover(
+            function() {
+                $(hov1).addClass('scheme-dark')
+            },
+            function() {
+                $(hov1).removeClass('scheme-dark')
+            }
+        );
+
+
+        var hov1item = document.querySelector('#scheme_01');
+        $('#scheme_item_01').hover(
+            function() {
+                $(hov1item).addClass('number_border')
+            },
+            function() {
+                $(hov1item).removeClass('number_border')
+            }
+        );
+        var openItem1 = document.querySelector('#scheme_01');
+
+        openItem1.addEventListener('click', function(evt) {
+            evt.preventDefault();
+
+            var deleteClass = document.querySelector('.scheme-dark-show');
+            var deleteNumber = document.querySelector('.number_border_adaptive');
+            deleteClass.classList.remove('scheme-dark-show');
+            deleteNumber.classList.remove('number_border_adaptive');
+
+            hov1.classList.add('scheme-dark-show');
+            openItem1.classList.add('number_border_adaptive');
+        });
+
+
+        var hov2 = document.querySelector('#scheme_item_02');
+        $('#scheme_02').hover(
+            function() {
+                $(hov2).addClass('scheme-dark')
+            },
+            function() {
+                $(hov2).removeClass('scheme-dark')
+            }
+        );
+
+        var hov2item = document.querySelector('#scheme_02');
+        $('#scheme_item_02').hover(
+            function() {
+                $(hov2item).addClass('number_border')
+            },
+            function() {
+                $(hov2item).removeClass('number_border')
+            }
+        );
+        var openItem2 = document.querySelector('#scheme_02');
+
+
+        openItem2.addEventListener('click', function(evt) {
+
+            evt.preventDefault();
+
+            var deleteClass = document.querySelector('.scheme-dark-show');
+            var deleteNumber = document.querySelector('.number_border_adaptive');
+            deleteClass.classList.remove('scheme-dark-show');
+            deleteNumber.classList.remove('number_border_adaptive');
+            hov2.classList.add('scheme-dark-show');
+            openItem2.classList.add('number_border_adaptive');
+        });
+
+        var hov3 = document.querySelector('#scheme_item_03');
+        $('#scheme_03').hover(
+            function() {
+                $(hov3).addClass('scheme-dark')
+            },
+            function() {
+                $(hov3).removeClass('scheme-dark')
+            }
+        );
+
+        var hov3item = document.querySelector('#scheme_03');
+        $('#scheme_item_03').hover(
+            function() {
+                $(hov3item).addClass('number_border')
+            },
+            function() {
+                $(hov3item).removeClass('number_border')
+            }
+        );
+        var openItem3 = document.querySelector('#scheme_03');
+
+
+        openItem3.addEventListener('click', function(evt) {
+
+            evt.preventDefault();
+
+            var deleteClass = document.querySelector('.scheme-dark-show');
+            var deleteNumber = document.querySelector('.number_border_adaptive');
+            deleteClass.classList.remove('scheme-dark-show');
+            deleteNumber.classList.remove('number_border_adaptive');
+            hov3.classList.add('scheme-dark-show');
+            openItem3.classList.add('number_border_adaptive');
+        });
+
+        var hov4 = document.querySelector('#scheme_item_04');
+        $('#scheme_04').hover(
+            function() {
+                $(hov4).addClass('scheme-dark')
+            },
+            function() {
+                $(hov4).removeClass('scheme-dark')
+            }
+        );
+
+        var hov4item = document.querySelector('#scheme_04');
+        $('#scheme_item_04').hover(
+            function() {
+                $(hov4item).addClass('number_border')
+            },
+            function() {
+                $(hov4item).removeClass('number_border')
+            }
+        );
+        var openItem4 = document.querySelector('#scheme_04');
+
+
+        openItem4.addEventListener('click', function(evt) {
+
+            evt.preventDefault();
+
+            var deleteClass = document.querySelector('.scheme-dark-show');
+            var deleteNumber = document.querySelector('.number_border_adaptive');
+            deleteClass.classList.remove('scheme-dark-show');
+            deleteNumber.classList.remove('number_border_adaptive');
+            hov4.classList.add('scheme-dark-show');
+            openItem4.classList.add('number_border_adaptive');
+        });
+        var hov5 = document.querySelector('#scheme_item_05');
+        $('#scheme_05').hover(
+            function() {
+                $(hov5).addClass('scheme-dark')
+            },
+            function() {
+                $(hov5).removeClass('scheme-dark')
+            }
+        );
+
+        var hov5item = document.querySelector('#scheme_05');
+        $('#scheme_item_05').hover(
+            function() {
+                $(hov5item).addClass('number_border')
+            },
+            function() {
+                $(hov5item).removeClass('number_border')
+            }
+        );
+        var openItem5 = document.querySelector('#scheme_05');
+
+
+        openItem5.addEventListener('click', function(evt) {
+
+            evt.preventDefault();
+
+            var deleteClass = document.querySelector('.scheme-dark-show');
+            var deleteNumber = document.querySelector('.number_border_adaptive');
+            deleteClass.classList.remove('scheme-dark-show');
+            deleteNumber.classList.remove('number_border_adaptive');
+            hov5.classList.add('scheme-dark-show');
+            openItem5.classList.add('number_border_adaptive');
+        });
+        var hov6 = document.querySelector('#scheme_item_06');
+        $('#scheme_06').hover(
+            function() {
+                $(hov6).addClass('scheme-dark')
+            },
+            function() {
+                $(hov6).removeClass('scheme-dark')
+            }
+        );
+
+        var hov6item = document.querySelector('#scheme_06');
+        $('#scheme_item_06').hover(
+            function() {
+                $(hov6item).addClass('number_border')
+            },
+            function() {
+                $(hov6item).removeClass('number_border')
+            }
+        );
+        var openItem6 = document.querySelector('#scheme_06');
+
+
+        openItem6.addEventListener('click', function(evt) {
+
+            evt.preventDefault();
+
+            var deleteClass = document.querySelector('.scheme-dark-show');
+            var deleteNumber = document.querySelector('.number_border_adaptive');
+            deleteClass.classList.remove('scheme-dark-show');
+            deleteNumber.classList.remove('number_border_adaptive');
+            hov6.classList.add('scheme-dark-show');
+            openItem6.classList.add('number_border_adaptive');
+        });
+
+        var hov7 = document.querySelector('#scheme_item_07');
+        $('#scheme_07').hover(
+            function() {
+                $(hov7).addClass('scheme-dark')
+            },
+            function() {
+                $(hov7).removeClass('scheme-dark')
+            }
+        );
+
+        var hov7item = document.querySelector('#scheme_07');
+        $('#scheme_item_07').hover(
+            function() {
+                $(hov7item).addClass('number_border')
+            },
+            function() {
+                $(hov7item).removeClass('number_border')
+            }
+        );
+
+        var openItem7 = document.querySelector('#scheme_07');
+
+
+        openItem7.addEventListener('click', function(evt) {
+            evt.preventDefault();
+            var deleteClass = document.querySelector('.scheme-dark-show');
+            var deleteNumber = document.querySelector('.number_border_adaptive');
+            deleteClass.classList.remove('scheme-dark-show');
+            deleteNumber.classList.remove('number_border_adaptive');
+            hov7.classList.add('scheme-dark-show');
+            openItem7.classList.add('number_border_adaptive');
+        });
+        var hov8 = document.querySelector('#scheme_item_08');
+        $('#scheme_08').hover(
+            function() {
+                $(hov8).addClass('scheme-dark')
+            },
+            function() {
+                $(hov8).removeClass('scheme-dark')
+            }
+        );
+
+        var hov8item = document.querySelector('#scheme_08');
+        $('#scheme_item_08').hover(
+            function() {
+                $(hov8item).addClass('number_border')
+            },
+            function() {
+                $(hov8item).removeClass('number_border')
+            }
+        );
+
+        var openItem8 = document.querySelector('#scheme_08');
+
+
+        openItem8.addEventListener('click', function(evt) {
+            evt.preventDefault();
+            var deleteClass = document.querySelector('.scheme-dark-show');
+            var deleteNumber = document.querySelector('.number_border_adaptive');
+            deleteClass.classList.remove('scheme-dark-show');
+            deleteNumber.classList.remove('number_border_adaptive');
+            hov8.classList.add('scheme-dark-show');
+            openItem8.classList.add('number_border_adaptive');
+        });
+        var hov9 = document.querySelector('#scheme_item_09');
+        $('#scheme_09').hover(
+            function() {
+                $(hov9).addClass('scheme-dark')
+            },
+            function() {
+                $(hov9).removeClass('scheme-dark')
+            }
+        );
+
+        var hov9item = document.querySelector('#scheme_09');
+        $('#scheme_item_09').hover(
+            function() {
+                $(hov9item).addClass('number_border')
+            },
+            function() {
+                $(hov9item).removeClass('number_border')
+            }
+        );
+
+        var openItem9 = document.querySelector('#scheme_09');
+        openItem9.addEventListener('click', function(evt) {
+            evt.preventDefault();
+            var deleteClass = document.querySelector('.scheme-dark-show');
+            var deleteNumber = document.querySelector('.number_border_adaptive');
+            deleteClass.classList.remove('scheme-dark-show');
+            deleteNumber.classList.remove('number_border_adaptive');
+            hov9.classList.add('scheme-dark-show');
+            openItem9.classList.add('number_border_adaptive');
+        });
